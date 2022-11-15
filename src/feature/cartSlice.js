b289026bc6ea0aea8082c987b17dd1667127a7bc
dipesh shakya <dipeshshakya106@gmail.com>
2022-11-15 22:54:28 +1100

    cart slice improve

--- src/feature/cartSlice.js
+++ src/feature/cartSlice.js
@@ -1,7 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
-  cartItem: [],
-  cartQty: 0,
+  cart: [],
 };
 
 export const cartSlice = createSlice({
@@ -9,14 +8,38 @@ export const cartSlice = createSlice({
   initialState,
   reducers: {
     addCart: (state, action) => {
-      state.cartItem.push(action.payload);
-      // eslint-disable-next-line no-unused-expressions
-      state.cartQty++;
+      const itemincart = state.cart.find(
+        (item) => item.id === action.payload.id
+      );
+      if (itemincart) {
+        itemincart.qty++;
+      } else {
+        state.cart.push({ ...action.payload, qty: 1 });
+      }
+    },
+    incrementQty: (state, action) => {
+      const item = state.cart.find((item) => item.id === action.payload);
+      item.quantity++;
+    },
+    decrementQty: (state, action) => {
+      const item = state.cart.find((item) => item.id === action.payload);
+      if (item.quantity === 1) {
+        item.quantity = 1;
+      } else {
+        item.quantity--;
+      }
+    },
+    removeItem: (state, action) => {
+      const removeItem = state.cart.filter(
+        (item) => item.id !== action.payload
+      );
+      state.cart = removeItem;
     },
   },
 });
 // const { actions, reducer } = cartSlice;
 // Extract and export each action creator by name
-export const { addCart } = cartSlice.actions;
+export const { addCart, incrementQty, decrementQty, removeItem } =
+  cartSlice.actions;
 // Export the reducer, either as a default or named export
 export default cartSlice.reducer;
