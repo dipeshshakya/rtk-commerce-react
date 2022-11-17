b397f048e2276a4eb00bde93247a082c89bc692c
dipesh shakya <dipeshshakya106@gmail.com>
2022-11-17 22:30:20 +1100

    persist store in localstorage

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from './store';
+import { store, persistor } from './store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
--- src/store.js
+++ src/store.js
@@ -1,9 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import ProductReducer from './feature/productSlice';
 import CartReducer from './feature/cartSlice';
-export const store = configureStore({
-  reducer: {
-    product: ProductReducer,
-    cart: CartReducer,
-  },
+import { persistStore, persistReducer } from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage';
+
+let rootReducer = combineReducers({
+  product: ProductReducer,
+  cart: CartReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
